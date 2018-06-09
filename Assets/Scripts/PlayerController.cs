6fa9b9dcfec9dc142d46905fd79322e2dc41d2c2
ArachnidMakr <38401091+ArachnidMakr@users.noreply.github.com>
2018-06-09 12:00:38 -0400

    Work Done

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+[RequireComponent(typeof(ConfigurableJoint))]
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
+    [SerializeField]
+    private float thrusterForce = 1000f;
+
+    [Header("Spring settings")]
+    [SerializeField]
+    private JointDriveMode jointMode = JointDriveMode.Position;
+    [SerializeField]
+    private float jointSpring = 20f;
+    [SerializeField]
+    private float jointMaxForce = 40f;
+
+
+    private PlayerMotor motor;
+    private ConfigurableJoint joint;
+
+	// Use this for initialization
+	void Start ()
+    {
+        motor = GetComponent<PlayerMotor>();
+        joint = GetComponent<ConfigurableJoint>();
+
+        SetJointSettings(jointSpring);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //calculate movement velocity as a 3d vector
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 movHorizontal = transform.right * xMov;
+        Vector3 movVertical = transform.forward * zMov;
+
+        //final movement vector
+        Vector3 velocity = (movHorizontal + movVertical).normalized * speed;
+
+        motor.Move(velocity);
+
+        //calculate rotation as a 3d vector
+        float yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 rotation = new Vector3(0f, yRot, 0f) * lookSensitivity;
+
+        //Apply rotation
+        motor.Rotate(rotation);
+
+        //calculate camera rotation as a 3d vector
+        float xRot = Input.GetAxisRaw("Mouse Y");
+
+        float _cameraRotation = xRot * lookSensitivity;
+
+        //Apply rotation
+        motor.RotateCamera(_cameraRotation);
+
+
+
+
+        //Calculate thrusterforce based on player input
+        Vector3 _thrusterForce = Vector3.zero;
+        //Apply thruster force
+        if (Input.GetButton("Jump"))
+        {
+            _thrusterForce = Vector3.up * thrusterForce;
+            SetJointSettings(0f);
+        }
+        else
+        {
+            SetJointSettings(jointSpring);
+        }
+
+        motor.ApplyThruster(_thrusterForce);
+    }
+
+    private void SetJointSettings(float _jointSpring)
+    {
+        joint.yDrive = new JointDrive
+        {
+            mode = jointMode,
+            positionSpring = jointSpring,
+            maximumForce = jointMaxForce
+        };
+    }
+
+}
---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(Component))]
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+    [SerializeField]
+    string remoteLayerName = "RemotePlayer";
+
+    Camera sceneCamera;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (!isLocalPlayer)
+        {
+            DisableComponents();
+            AssignRemoteLayer();
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+            
+        }
+
+        RegisterPlayer();
+	}
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        string _netID = GetComponent<NetworkIdentity>().netId.ToString();
+        Player _player = GetComponent<Player>();
+
+        GameManager.RegisterPlayer(_netID, _player);
+    }
+
+    void RegisterPlayer()
+    {
+        string _ID = "Player " + GetComponent<NetworkIdentity>().netId;
+        transform.name = _ID;
+    }
+
+    void AssignRemoteLayer()
+    {
+        gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+    }
+
+    private void OnDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+
+        GameManager.UnRegisterPlayer(transform.name);
+    }
+    void DisableComponents()
+    {
+        for (int i = 0; i < componentsToDisable.Length; i++)
+        {
+            componentsToDisable[i].enabled = false;
+        }
+    }
+
+    
+
+}
